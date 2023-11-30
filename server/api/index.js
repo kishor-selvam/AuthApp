cd3e4d5ddd98c2a13e6e35803378208a488fd179
Kishor S <kishorsbca@gmail.com>
2023-11-30 16:27:22 +0530

    Added Router and Controller in Backend

--- server/api/index.js
+++ server/api/index.js
@@ -1,16 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import UserRoutes from "../api/routes/user.route.js";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to mongodb");
-  }).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
   });
-  
+
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000!!!");
 });
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Api is Working",
+  });
+});
+
+app.use("/api/user", UserRoutes);
