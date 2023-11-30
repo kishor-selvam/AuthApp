90ab09feff6c30df35fcbcea5945b785f580009f
Kishor S <kishorsbca@gmail.com>
2023-11-30 16:16:16 +0530

    create user model

--- server/api/index.js
+++ server/api/index.js
@@ -2,15 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
- 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
+
+mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to mongodb");
-  })
-  .catch((err) => {
+  }).catch((err) => {
     console.log(err);
   });
+  
 const app = express();
 
 app.listen(3000, () => {
