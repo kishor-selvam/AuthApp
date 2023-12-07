04d0016bf448e3953b3b143b51c8896d4026a733
Kishor S <kishorsbca@gmail.com>
2023-12-07 18:44:14 +0530

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,6 +9,9 @@ import {
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import {
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
   updateUserFailure,
   updateUserStart,
   updateUserSuccess,
@@ -79,6 +82,23 @@ const Profile = () => {
     }
   };
 
+  const handleDeleteAccount = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7 ">Profile</h1>
@@ -137,12 +157,15 @@ const Profile = () => {
         </button>
       </form>
       <div className="flex justify-between mt-5 px-1">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span
+          onClick={handleDeleteAccount}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete Account
+        </span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
-      <p className="text-red-700 mt-5">
-        {error ? error.message : "Something went wrong!"}
-      </p>
+      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
       <p className="text-green-700 mt-5">
         {updateSuccess && "User is updated successfully!"}
       </p>
--- server/api/routes/user.route.js
+++ server/api/routes/user.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { test, updateUser } from "../controller/user.controller.js";
+import { test, updateUser, deleteuser } from "../controller/user.controller.js";
 import { verifyToken } from "../../util/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/", test);
 router.post("/update/:id", verifyToken, updateUser);
-
+router.delete("/delete/:id", verifyToken, deleteuser);
 export default router;
